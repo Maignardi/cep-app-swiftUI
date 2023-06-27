c484820df61b40ab3113ce2a52989af360488ed1
MATHEUS MENDONCA MAIGNARDI <70504309+Maignardi@users.noreply.github.com>
2023-06-27 17:42:14 -0400

    Initial Commit

---
+++ cep-app-pxBank/cep_app_pxBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  cep_app_pxBankApp.swift
+//  cep-app-pxBank
+//
+//  Created by matheus maignardi on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct cep_app_pxBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
